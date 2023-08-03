7a1e93cce14649f0dbfd0404b86385c25ff774a5
Mwai Banda <mwai.banda@cbn.org>
2023-08-02 21:08:46 -0500

    Added prisma and schema phase::1

--- go.mod
+++ go.mod
@@ -2,7 +2,15 @@ module Momentum
 
 go 1.20
 
-require github.com/gofiber/fiber/v2 v2.48.0
+require (
+	github.com/gofiber/fiber/v2 v2.48.0
+	github.com/iancoleman/strcase v0.0.0-20190422225806-e506e3ef7365
+	github.com/joho/godotenv v1.5.1
+	github.com/shopspring/decimal v1.3.1
+	github.com/steebchen/prisma-client-go v0.21.0
+	github.com/takuoki/gocase v1.0.0
+	golang.org/x/text v0.10.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
--- main.go
+++ main.go
@@ -1,26 +1,62 @@
 package main
 
 import (
+	"Momentum/prisma/db"
+	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-type Hello struct {
-	Text string `json:"text"`
+type Post struct {
+	Id        string `json:"id"`
+	CreatedAt string `json:"createdAt"`
+	UpdatedAt string `json:"updatedAt"`
+	Title     string `json:"title"`
+	Published bool   `json:"published"`
+	desc      string `json:"desc"`
 }
 
 func main() {
 	app := fiber.New()
-	res, err := json.Marshal(Hello{Text: "Hello World"})
-	if err != nil {
-		log.Fatal(err.Error())
+	client := db.NewClient()
+	if err := client.Prisma.Connect(); err != nil {
+		fmt.Println(err)
 	}
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Send(res)
+	defer func() {
+		if err := client.Prisma.Disconnect(); err != nil {
+			panic(err)
+		}
+	}()
+
+	ctx := context.Background()
+
+	app.Get("/", func(context *fiber.Ctx) error {
+		demo := Post{Title: "Hello World"}
+		_, err := json.Marshal(demo)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return context.JSON(demo)
+	})
+
+	app.Get("/posts", func(context *fiber.Ctx) error {
+		var posts []Post
+		post, err := client.Post.FindMany().Exec(ctx)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		result, _ := json.MarshalIndent(post, "", "  ")
+		if err := json.Unmarshal(result, &posts); err != nil {
+			fmt.Println(err)
+		}
+
+		return context.JSON(posts)
 	})
 
 	port := os.Getenv("PORT")
